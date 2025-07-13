cec79d116895441edaab6149159c03f10f9af293
Sam <sambhavchugh2004@gmail.com>
2025-07-13 17:15:19 +0530

    qwer

--- script.js
+++ script.js
@@ -41,6 +41,37 @@ var splide = new Splide('.Team_Splide', {
 splide.mount();
 
 
+
+const heroWrapper = document.querySelector(".hero_Wrapper");
+const fadeOverlay = document.querySelector(".fade_overlay");
+let currentIndex = 0;
+
+const images = [
+  "media_library_export-bharat_chugh_designs-2025_07_13_05_29_58/farmhouse-at-Nuh.jpg",
+  "media_library_export-bharat_chugh_designs-2025_07_13_05_29_58/Sandstone-Budha-sculpture.jpg",
+  "media_library_export-bharat_chugh_designs-2025_07_13_05_29_58/Sheikh-Nahayan-Mosque-Abu-Dhabi.jpg",
+  "media_library_export-bharat_chugh_designs-2025_07_13_05_29_58/scared-altars-and-mandirs.jpg"
+];
+
+const nextImgBtn = document.querySelector(".hero_side_button");
+
+function changeImage() {
+  fadeOverlay.classList.add("active");
+
+  setTimeout(() => {
+    currentIndex = (currentIndex + 1) % images.length;
+    heroWrapper.style.backgroundImage = `url('${images[currentIndex]}')`;
+    fadeOverlay.classList.remove("active");
+  }, 600); // Match transition time
+}
+
+// Manual trigger
+nextImgBtn.addEventListener('click', changeImage);
+
+// Auto-slide every 5 seconds
+setInterval(changeImage, 10000);
+
+
 let client_link1 = document.querySelector('.client_link1')
 let client_link2 = document.querySelector('.client_link2')
 let client_link3 = document.querySelector('.client_link3')
@@ -99,3 +130,7 @@ nav_open.addEventListener('click', () => {
 nav_close.addEventListener('click', () => {
     menu.classList.remove('show_menu');
 })
+
+
+
+
